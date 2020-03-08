8bf1782fa1e93ade1ac51d555d1c30ea9ac8fa06
RenataMalek <renatamalek@yahoo.com.br>
2020-03-08 01:30:09 -0300

    Versão .0

--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -1,3 +1,7 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.swing.JOptionPane;
 
 public class Conta {
@@ -5,15 +9,16 @@ public class Conta {
 	double saldo = 1000.00;
 	String numero = "1234";
 	String senha = "123456";
+	Transacao historico = new Transacao();
 
 	public void chamarMenu() {
 		double valor;
 		String codigoBarras;
-		int opc = 0;
+		int opc = 0, aux;
 
 		while (opc != 9) {
-			opc = Integer.parseInt(JOptionPane
-					.showInputDialog("*ESCOLHA A OPÇÃO DESEJADA*\n1 - Ver saldo \n2 - Efetuar Saque "
+			opc = Integer.parseInt(
+					JOptionPane.showInputDialog("*ESCOLHA A OPÇÃO DESEJADA*\n1 - Ver saldo \n2 - Efetuar Saque "
 							+ "\n3 - Efetuar Depósito \n4 - Efetuar Pagamentos \n5 - Ver extrato \n9 - Encerrar"));
 
 			switch (opc) {
@@ -46,9 +51,23 @@ public class Conta {
 					JOptionPane.showMessageDialog(null, "Saldo insuficiente");
 				}
 				break;
-			case 5: // extrato();
+			case 5:
+				aux = Integer.parseInt(JOptionPane.showInputDialog(
+						"Escolha o período de histórico para emissão do extrato \n1 - Ultimas 24 horas \n2 - Ultimos 5 dias \n3 - "
+								+ "Ultimos 30 dias \n9 - Voltar menu anterior"));
+
+				if (aux == 1) {
+					historico.exibirHistorico(1);
+				} else if (aux == 2) {
+					historico.exibirHistorico(5);
+				} else if (aux == 3) {
+					historico.exibirHistorico(30);
+				} else {
+					JOptionPane.showMessageDialog(null, "INVALIDO, TENTE NOVAMENTE");
+				}
 				break;
-			case 9: JOptionPane.showMessageDialog(null, "OPERAÇÃO FINALIZADA");
+			case 9:
+				JOptionPane.showMessageDialog(null, "OPERAÇÃO FINALIZADA");
 				System.exit(0);
 				break;
 			default:
@@ -62,22 +81,48 @@ public class Conta {
 
 	public void saque(double valor) {
 
+		Transacao enviarSaque = new Transacao();
+
 		saldo -= valor;
 		JOptionPane.showMessageDialog(null, "Retire o valor");
 
+		Date data = new Date();
+		enviarSaque.historicoSaque(valor, data);
 	}
 
 	public void deposito(double valor) {
 
+		Transacao enviarDeposito = new Transacao();
+
 		saldo += valor;
 		JOptionPane.showMessageDialog(null, "Depósito realizado");
+
+		Date data = new Date();
+		enviarDeposito.historicoDeposito(valor, data);
 
 	}
 
 	public void pagamento(String codigo, double valor) {
 
+		Transacao enviarPgto = new Transacao();
+
 		saldo -= valor;
 		JOptionPane.showMessageDialog(null, "Pagamento realizado");
 
+		Date data = new Date();
+		enviarPgto.historicoPagamento(valor, data);
+
 	}
+
+	public Calendar recebeData(double valor) {
+
+		Date data = new Date();
+		Calendar captar = new GregorianCalendar();
+
+		captar.setTime(data);
+
+		return (captar);
+
+	}
+
 }
---
+++ Banco/src/Transacao.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Transacao {
+	double valor;
+	Date data;
+
+	
+	public void exibirHistorico(int aux) {
+		
+		
+		
+		
+		
+	}
+	
+	public ArrayList<Transacao> historicoSaque(double valor, Date data) {
+
+		Transacao recebeSaque = new Transacao();
+
+		recebeSaque.valor = valor;
+		recebeSaque.data = data;
+
+		ArrayList<Transacao> saque = new ArrayList<Transacao>();
+
+		saque.add(recebeSaque);
+		
+		return (saque);
+	}
+
+	public ArrayList<Transacao> historicoDeposito(double valor, Date data) {
+
+		Transacao recebeDeposito = new Transacao();
+
+		recebeDeposito.valor = valor;
+		recebeDeposito.data = data;
+
+		ArrayList<Transacao> deposito = new ArrayList<Transacao>();
+
+		deposito.add(recebeDeposito);
+		
+		return (deposito);
+	}
+
+	public ArrayList<Transacao> historicoPagamento(double valor, Date data) {
+		Transacao recebePgto = new Transacao();
+
+		recebePgto.valor = valor;
+		recebePgto.data = data;
+
+		ArrayList<Transacao> pgto = new ArrayList<Transacao>();
+
+		pgto.add(recebePgto);
+		
+		return (pgto);
+	}
+
+}
